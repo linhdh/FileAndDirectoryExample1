d5649c064728a11a92abb4aac3f1729c5ef3910f
Dao Hai Linh <linhdh@live.com>
2023-01-03 15:03:31 +0700

    initial commit.

---
+++ FileAndFolderExample1/FileAndFolderExample1/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace FileAndFolderExample1
+{
+    internal class Program
+    {
+        static readonly string _file = "C:\\Windows\\Temp\\Updater_v1.3.21.zip";
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Path.GetFileNameWithoutExtension(_file));
+            Console.WriteLine(Path.GetFullPath(_file));
+            Console.WriteLine(Path.GetFileName(_file));
+            Console.WriteLine(Path.GetDirectoryName(_file));
+            Console.WriteLine(Path.Combine(Path.GetDirectoryName(_file), Path.GetFileNameWithoutExtension(_file)));
+        }
+    }
+}
